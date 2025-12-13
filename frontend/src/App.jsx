a0da59e1b25b48a18b66edfadc45fa48fb499a27
milenchaves <milenchaves.study@gmail.com>
2025-12-13 10:44:34 -0300

    fix: corrigindo rota home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import GamesList from "./pages/games/GamesList";
 import Header from "./components/Header";
 import TutorialsList from "./pages/tutorials/TutorialsList";
