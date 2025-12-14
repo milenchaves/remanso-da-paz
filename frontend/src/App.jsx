416799c8683b2650a21e23113c6aa671ab101e6b
Tatiely Nobre <tatielylima@alu.ufc.br>
2025-12-14 15:03:58 -0300

    Revert "fix: scroll"
    This reverts commit 928d1558bba7cc44a93124f52ae6e2021d88cfeb.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
 import GamesList from "./pages/games/GamesList";
+import Header from "./components/Header";
 import TutorialsList from "./pages/tutorials/TutorialsList";
 import TutorialDetail from "./pages/tutorials/TutorialDetail";
 import OddOneOut from "./pages/games/oddOneOut/OddOneOut";
@@ -10,20 +10,19 @@ import OddOneOut from "./pages/games/oddOneOut/OddOneOut";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          
+      <Header />
+      <main className="content">
+        <Routes>
           <Route path="/" element={<Home />} />
-          
-          {/* Rotas de Jogos */}
           <Route path="/games" element={<GamesList />} />
           <Route path="/games/odd-one-out" element={<OddOneOut />} />
-          <Route path="/games/DrawerOrganizer" element={<DrawerOrganizer/>} />
+          <Route path="/games/memory-game" element={<MemoryGame />} />
+          
+          {/* Rotas de Tutoriais */}
           <Route path="/tutorials" element={<TutorialsList />} />
           <Route path="/tutorials/:id" element={<TutorialDetail />} />
-          
-        </Route>
-      </Routes>
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
