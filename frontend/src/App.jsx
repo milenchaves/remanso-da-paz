519b62d4de7e9a3f992a2123fe0f9ba5b5bfdd7c
letdnts <mleticiadantas@alu.ufc.br>
2025-12-14 16:25:28 -0300

    fix: scroll v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
 import GamesList from "./pages/games/GamesList";
-import Header from "./components/Header";
 import TutorialsList from "./pages/tutorials/TutorialsList";
 import TutorialDetail from "./pages/tutorials/TutorialDetail";
 import OddOneOut from "./pages/games/oddOneOut/OddOneOut";
 import TypingGame from "./pages/games/typing/TypingGame";
 import MemoryGame from "./pages/games/memoryGame/MemoryGame";
+
 function App() {
   return (
     <BrowserRouter>
@@ -18,7 +18,8 @@ function App() {
           <Route path="/games" element={<GamesList />} />
           <Route path="/games/memoryGame" element={<MemoryGame />} />
           <Route path="/games/odd-one-out" element={<OddOneOut />} />
-          <Route path="/games/typing" element={<TypingGame />} />          
+          <Route path="/games/typing" element={<TypingGame />} />   
+          
           {/* Rotas de Tutoriais */}
           <Route path="/tutorials" element={<TutorialsList />} />
           <Route path="/tutorials/:id" element={<TutorialDetail />} />
