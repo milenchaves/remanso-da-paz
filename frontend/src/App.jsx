7b269e291f0ad34c5ee07d19d6730cdd30ec29c6
milenchaves <milenchaves.study@gmail.com>
2025-12-14 18:26:36 -0300

    fix: adicionando footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
 import GamesList from "./pages/games/GamesList";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header"; 
 import TutorialsList from "./pages/tutorials/TutorialsList";
 import TutorialDetail from "./pages/tutorials/TutorialDetail";
 import OddOneOut from "./pages/games/oddOneOut/OddOneOut";
+import TypingGame from "./pages/games/typing/TypingGame";
 import MemoryGame from "./pages/games/memoryGame/MemoryGame";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main className="content">
-        <Routes>
+      <Routes>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/games" element={<GamesList />} />
+          <Route path="/games/memoryGame" element={<MemoryGame />} />
           <Route path="/games/odd-one-out" element={<OddOneOut />} />
-          <Route path="/games/memory-game" element={<MemoryGame />} />
+          <Route path="/games/typing" element={<TypingGame />} />          
+          {/* Rotas de Tutoriais */}
           <Route path="/tutorials" element={<TutorialsList />} />
           <Route path="/tutorials/:id" element={<TutorialDetail />} />
-        </Routes>
-      </main>
-      <Footer />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
