f71406951ccac29e26cb25c519f720f9406d0c3d
Tatiely Nobre <tatielylima@alu.ufc.br>
2025-12-14 14:22:58 -0300

    Revert "feat: jogo: DrawerOganizer e header"
    This reverts commit 415910d178e5a7e9e55464a56c151370208b43b7.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,6 @@ import GamesList from "./pages/games/GamesList";
 import TutorialsList from "./pages/tutorials/TutorialsList";
 import TutorialDetail from "./pages/tutorials/TutorialDetail";
 import OddOneOut from "./pages/games/oddOneOut/OddOneOut";
-<<<<<<< HEAD
-import DrawerOrganizer from "./pages/games/DrawerOrganizer/DrawerOrganizer";
-=======
-import MemoryGame from "./pages/games/memoryGame/MemoryGame";
->>>>>>> 928d1558bba7cc44a93124f52ae6e2021d88cfeb
 
 function App() {
   return (
@@ -23,13 +18,7 @@ function App() {
           {/* Rotas de Jogos */}
           <Route path="/games" element={<GamesList />} />
           <Route path="/games/odd-one-out" element={<OddOneOut />} />
-<<<<<<< HEAD
           <Route path="/games/DrawerOrganizer" element={<DrawerOrganizer/>} />
-=======
-          <Route path="/games/memory-game" element={<MemoryGame />} />
-          
-          {/* Rotas de Tutoriais */}
->>>>>>> 928d1558bba7cc44a93124f52ae6e2021d88cfeb
           <Route path="/tutorials" element={<TutorialsList />} />
           <Route path="/tutorials/:id" element={<TutorialDetail />} />
           
